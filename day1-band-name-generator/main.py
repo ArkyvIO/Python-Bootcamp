3366a2898be662ef342a7ac46d8d84edd9ef3103
ArkyvIO <arkyvio@gmail.com>
2023-04-16 14:49:31 -0400

    Day 1 complete

---
+++ day1-band-name-generator/main.py
@@ -0,0 +1,7 @@
+# Greeting
+print("BAND NAME GENERATOR")
+# Set variables
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What is the name of your pet?\n")
+# Generate band name
+print("You band name is " + city + " " + pet)
