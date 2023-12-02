7a47d72e9a4ceaff0031a9a3c3b9fa2595377c68
ArkyvIO <arkyvio@gmail.com>
2023-12-02 14:50:27 -0500

    Created question class for object to hold ? & ans

---
+++ day17-quiz-true-false/question_model.py
@@ -0,0 +1,9 @@
+# Create object for questions
+class Question:
+    # Initialize each question with question and answer
+    def __init__(self, question, answer) -> None:
+        # Set question to question
+        self.question = question
+        # Set answer to answer
+        self.answer = answer
+
