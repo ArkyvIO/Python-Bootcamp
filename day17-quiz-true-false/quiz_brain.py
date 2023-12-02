254f1f5eeb881723271393547521c04d0e93b959
ArkyvIO <arkyvio@gmail.com>
2023-12-02 15:46:10 -0500

    Check user answer with validation

--- day17-quiz-true-false/quiz_brain.py
+++ day17-quiz-true-false/quiz_brain.py
@@ -1,12 +1,31 @@
 class QuizBrain:
+    
+    # Initialize base object, has question num, list and score
     def __init__(self, question_list):
         self.question_number = 0
         self.question_list = question_list
+        self.score = 0
         
+    # Get next question in object list
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"Question {self.question_number}: {current_question.question} --- True or False: ")
+        user_answer = input(f"Question {self.question_number}: {current_question.question} --- True or False: ")
+        # User input validation, should always be t or f for first letter of answer
+        while True:
+            if user_answer[0].lower() == "t" or user_answer[0].lower() == "f":
+                break
+            else:
+                user_answer = input("That input was not recognized, please try again: ")
+        self.check_answer(user_answer, current_question.answer)
         
-    def still_has_questions(self, question_list):
+    # Check if the list still has questions
+    def still_has_questions(self):
         return self.question_number < len(self.question_list)
+    
+    # Check if the answer was correct or incorrect.
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer[0].lower() == correct_answer[0].lower():
+            print("Correct!")
+        else:
+            print("Incorrect!")
