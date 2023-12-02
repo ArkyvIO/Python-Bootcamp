0b7ece3820bb77987109e7c31a06162319256ec0
ArkyvIO <arkyvio@gmail.com>
2023-12-02 18:16:37 -0500

    Spirograph coded, set speeds for each painting

--- day18-turtle-and-gui/defs.py
+++ day18-turtle-and-gui/defs.py
@@ -1,11 +1,13 @@
 from random import random, choice
 
 def draw_square(turtle):
+    turtle.speed(3)
     for _ in range(4):
         turtle.forward(100)
         turtle.right(90)
         
 def draw_dashed_line(turtle, line_length):
+    turtle.speed(3)
     for _ in range(int(line_length / 10)):
         turtle.forward(10)
         turtle.up()
@@ -13,6 +15,7 @@ def draw_dashed_line(turtle, line_length):
         turtle.down()
         
 def draw_increasing_shapes(turtle, iterations):
+    turtle.speed(5)
     for sides in range(3, iterations + 3):
         turtle.color(random(), random(), random())
         for _ in range(sides):
@@ -21,6 +24,7 @@ def draw_increasing_shapes(turtle, iterations):
         sides += 1
 
 def draw_random_walk(turtle, steps):
+    turtle.speed(5)
     for _ in range(steps):
         turtle.color(random(), random(), random())
         turtle.forward(25)
@@ -31,3 +35,10 @@ def draw_random_walk(turtle, steps):
             turtle.right(90)
         else:
             turtle.left(90)
+            
+def draw_spirograph(turtle, radius, circles):
+    turtle.speed(0)
+    for _ in range(circles):
+        turtle.color(random(), random(), random())
+        turtle.circle(radius)
+        turtle.left(360 / circles)
--- day18-turtle-and-gui/main.py
+++ day18-turtle-and-gui/main.py
@@ -1,14 +1,14 @@
 from turtle import Turtle, Screen
-from defs import draw_square, draw_dashed_line, draw_increasing_shapes, draw_random_walk
+from defs import draw_square, draw_dashed_line, draw_increasing_shapes, draw_random_walk, draw_spirograph
 
 # Create turtle
 turtle = Turtle()
 # Make the turtle an arrow
 turtle.shape("arrow")
 # Turtle line thickness, default 1
-turtle.pensize(10)
+turtle.pensize(5)
 # Turtle speed
-turtle.speed(8)
+turtle.speed(0)
 
 # Draw a square
 # draw_square(turtle)
@@ -20,7 +20,10 @@ turtle.speed(8)
 # draw_increasing_shapes(turtle, 10)
 
 # Draw random with step count
-draw_random_walk(turtle, 100)
+# draw_random_walk(turtle, 100)
+
+# Draw a spirograph
+draw_spirograph(turtle, 250, 50)
 
 # Create screen
 screen = Screen()
